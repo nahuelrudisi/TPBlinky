c98d410928d19d95b2765cce4d965ffe9abcf822
nahuelrudisi <nahuelrudisi@hotmail.com>
2016-09-07 21:24:51 -0300

    Laboratorio 1 FINAL 2

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 //	TABLA DE VERDAD Y SECUENCIA EN QUE ENCIENDEN LOS LEDS
-//	R	G	B
-//	0	0	0
-//	0	0	1
-//	0	1	1
-//	1	1	1
-//	1	1	0
-//	1	0	0
-//	1	0	1
-//	0	1	0
+//		R		G		B
+//		0		0		0
+//		0		0		1
+//		0		1		1
+//		1		1		1
+//		1		1		0
+//		1		0		0
+//		1		0		1
+//		0		1		0
 
 #include "bsp/bsp.h"
 
@@ -16,7 +16,7 @@
 int main(void)
 {
 	int i;
-	float adc[5],menor,calculo;
+	float adc[5],menor;
 	uint32_t potenciometro = 0;
 	uint32_t valor_ROJO = 0, valor_VERDE = 0, valor_AZUL = 0;
 	BSP_Init();
@@ -27,67 +27,58 @@ int main(void)
 		{
 			adc[i] = BSP_GetBrightness();
 			if ((i == 0) || (adc[i] < menor))
-				menor = adc[i];
+				menor = adc[i];						// Saco el menor para que no me afecte la variacion del ADC
 		}
 		menor = menor * multiplicador;
 		potenciometro = (uint32_t) menor;
 
 		if(potenciometro <= 14285714)
 		{
-			calculo = 0.000007 * potenciometro;
 			valor_ROJO  = 0;
 			valor_VERDE = 0;
-			valor_AZUL  = (uint32_t) calculo;
+			valor_AZUL  = (uint32_t) (0.000007 * potenciometro);
 		}
 
 		if((potenciometro > 14285714) && (potenciometro <= 28571428))
 		{
-			calculo = 0.000007 * potenciometro - 100;
 			valor_ROJO  = 0;
-			valor_VERDE = (uint32_t) calculo;
+			valor_VERDE = (uint32_t) (0.000007 * potenciometro - 100);
 			valor_AZUL  = 100;
 		}
 
 		if((potenciometro > 28571428) && (potenciometro <= 42857142))
 		{
-			calculo = 0.000007 * potenciometro - 200;
-			valor_ROJO  = (uint32_t) calculo;
+			valor_ROJO  = (uint32_t) (0.000007 * potenciometro - 200);
 			valor_VERDE = 100;
 			valor_AZUL  = 100;
 		}
 
 		if((potenciometro > 42857142) && (potenciometro <= 57142857))
 		{
-			calculo = -0.000007 * potenciometro + 400;
 			valor_ROJO  = 100;
 			valor_VERDE = 100;
-			valor_AZUL  = (uint32_t) calculo;
+			valor_AZUL  = (uint32_t) (-0.000007 * potenciometro + 400);
 		}
 
 		if((potenciometro > 57142857) && (potenciometro <= 71428571))
 		{
-			calculo = -0.000007 * potenciometro + 500;
 			valor_ROJO  = 100;
-			valor_VERDE = (uint32_t) calculo;
+			valor_VERDE = (uint32_t) (-0.000007 * potenciometro + 500);
 			valor_AZUL  = 0;
 		}
 
 		if((potenciometro > 71428571) && (potenciometro <= 85714286))
 		{
-			calculo = 0.000007 * potenciometro - 500;
 			valor_ROJO  = 100;
 			valor_VERDE = 0;
-			valor_AZUL  = (uint32_t) calculo;
+			valor_AZUL  = (uint32_t) (0.000007 * potenciometro - 500);
 		}
 
 		if((potenciometro > 85714286) && (potenciometro <= 100000000))
 		{
-			calculo = -0.000007 * potenciometro + 700;
-			valor_ROJO  = (uint32_t) calculo;
-			calculo = 0.000007 * potenciometro - 600;
-			valor_VERDE = (uint32_t) calculo;
-			calculo = -0.000007 * potenciometro + 700;
-			valor_AZUL  = (uint32_t) calculo;
+			valor_ROJO  = (uint32_t) (-0.000007 * potenciometro + 700);
+			valor_VERDE = (uint32_t) (0.000007 * potenciometro - 600);
+			valor_AZUL  = (uint32_t) (-0.000007 * potenciometro + 700);
 		}
 
 		RGB_PWM(RGB_AZUL, valor_AZUL);
